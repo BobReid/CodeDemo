fd9f448ae1c5bea2cafcc42c8124afd78d018414
Robert Reid <robert.reid@jda.com>
2013-10-24 19:09:22 -0400

    Initial Commit

---
+++ CodeDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CodeDemo
+//
+//  Created by Bob Reid on 10/24/2013.
+//  Copyright (c) 2013 Bob Reid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDAppDelegate class]));
+    }
+}
